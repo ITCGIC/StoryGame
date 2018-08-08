823c7dcf2787c3a49d8f8b19cb39bc6e7082ec42
SrengMSI <srengty@gmail.com>
2018-08-08 15:06:19 +0700

    add Horror 1, 1.1, 1.2

--- app/src/main/java/gictitc/storygame/storygame/HorrorActivity.java
+++ app/src/main/java/gictitc/storygame/storygame/HorrorActivity.java
@@ -1,34 +1,29 @@
 package gictitc.storygame.storygame;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HorrorActivity extends AppCompatActivity {
+public class HorrorActivity extends BaseGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horror);
     }
-    @Override
-    public void onBackPressed() {
-        AlertDialog alertDialog = new AlertDialog.Builder(this)
-                .setTitle("Confirm")
-                .setMessage("Are you sure leaving the game?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                }).create();
-        alertDialog.show();
+
+    public void checkItOut(View view) {
+        Intent intent = new Intent(this, Hprror1_1Activity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void keepGoing(View view) {
+        Intent intent = new Intent(this, Hprror1_2Activity.class);
+        startActivity(intent);
+        finish();
     }
 }
